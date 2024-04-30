e563d1764ee59353c9fec37c89644c651f7c28be
oleno <es.moname@gmail.com>
2024-04-30 10:04:31 +0200

    add task-item component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
+  
+  @Input() text:string = ''; 
+  @Input() color:string = '';
+  @Output() btnClick = new EventEmitter();
 
+  onClick(){
+    this.btnClick.emit();
+  }
 }
---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+  
+  @Input() task: Task = {
+    text: '',
+    day: '',
+    reminder: false
+  };
+
+}
